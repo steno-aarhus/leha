4cdbcd75c40be3fef120f3b9dd250352d16479fb
Fie Langmann <fielangmann@gmail.com>
2023-09-18 12:28:37 +0000

    added code to create a tibble for data management and analyses

--- R/data_start.R
+++ R/data_start.R
@@ -6,3 +6,7 @@ library(here)
 # If you ever want to convert the dataset into a "tibble"/"dataframe", use
 # `tibble::as_tibble()`.
 data <- ukbAid::read_parquet(here("data/data.parquet"))
+
+
+# Converting the dataset into a tibble to work with for analyses
+data <-tibble::as_tibble(data)
