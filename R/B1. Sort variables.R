34f691af312964de69ec26c59b6eb7949b998332
Fie Langmann <fielangmann@gmail.com>
2023-10-12 11:50:39 +0000

    Code for matching ICD10 codes and dates added

--- R/B1. Sort variables.R
+++ R/B1. Sort variables.R
@@ -229,44 +229,71 @@ test <- data %>%
     select(p41270, starts_with("p41280")) %>%
     slice(1:100)
 
-# Split the 'variabel into separate variables based on delimiter "|"
+# Split the 'variable into separate variables based on delimiter "|"
 library(tidyr)
-
-test1 <- test %>%
+test <- test %>%
   separate_wider_delim(p41270, delim = "|",
                        names_sep = "var_a", too_few = "debug")
 
-test2 <- data %>%
-  separate_wider_delim(p41270, delim = "|",
-                       names_sep = "var_a", too_few = "debug")
+# When code works, uncomment below and split variable for all observations
+# data <- data %>%
+#   separate_wider_delim(p41270, delim = "|",
+#                        names_sep = "var_a", too_few = "debug")
+
+# Pivot_longer code
+# long_test <- test %>%
+#   pivot_longer(cols = matches("_a[0-259]$"),
+#                names_to = c(".value", "a"),
+#                names_sep = "_")
+
+# When code works, uncomment below and pivot_longer
+# long_data <- data %>%
+#   pivot_longer(cols = matches("_a[0-259]$"),
+#                names_to = c(".value", "a"),
+#                names_sep = "_")
 
 
-long_test1 <- test2 %>%
-  pivot_longer(cols = matches("var_a"),
-               names_to = c(".value", "a"),
-               names_sep = "_")
+# In lack of better description, I want to do this:
+# if any starts_with("p41270") and ends with "_a[0-259]" includes "K80" or "K81",
+# include any p41280 than ends with the same "_a[0-259]". Variable content not matching
+# these criteria should be changed to "NA"
+
+# Define a vector of suffixes to match
+suffixes_to_match <- paste0("_a", 0:259)
+
+# Apply the logic to update the variables
+df <- test %>%
+  rowwise() %>%
+  mutate(
+    across(starts_with("p41270"), .fns = ~ {
+      case_when(
+        grepl("K80|K81", .) & grepl(paste0("(", paste(suffixes_to_match, collapse = "|"), ")$"), cur_column()) ~ .,
+        TRUE ~ "NA"
+      )
+    }),
+    across(starts_with("p41280"), .fns = ~ {
+      case_when(
+        grepl(paste0("(", paste(suffixes_to_match, collapse = "|"), ")$"), cur_column()) ~ .,
+        TRUE ~ "NA"
+      )
+    })
+  ) %>%
+  ungroup()
 
 
-long_test <- test1 %>%
-  pivot_longer(cols = matches("var_a[0-258]$"),
-               names_to = c(".value", "a"),
-               names_sep = "_")
+In this modified code, we use rowwise() to process each row individually, which resolves the recycling error. The cur_column() function is used to get the name of the current column within the across() function. After processing the data rowwise, we use ungroup() to return to the standard data frame structure.
 
-long_test <- test1 %>%
-  pivot_longer(cols = matches("_a[0-258]$"),
-               names_to = c(".value", "a"),
-               names_sep = "_")
-
-Det kunne være fint med en funktion som kunne definere names automatisk ud fra antal splits i var1.
-
-              if p41270 includes "K80" or "K81", include any p41280 arrays that matches this
-              if p41271 includes starts_with"574" or "5750" or "5751", include any p41281 arrays that matches this
-              if p41272 includes "J18", "J21.1 ", "J24.2", J24.3 J26.1, include any p41282 arrays that matches this
-
-              Removal of gallbladder
+This should apply the logic you described without the error you encountered.
 
 
-              p40000 date of death - two instances - should be combined
+
+# When code works it should be rerun for ICD9 and OPSC4 codes below:
+# if p41271 includes starts_with"574" or "5750" or "5751", include any p41281 arrays that matches this
+# if p41272 includes "J18", "J21.1 ", "J24.2", J24.3 J26.1, include any p41282 arrays that matches this
+
+
+
+# p40000_i0 date of death
 
               # How can I combine these?
               p41270,Diagnoses - ICD10,440017,https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=41270 # delete?
@@ -276,6 +303,6 @@ Det kunne være fint med en funktion som kunne definere names automatisk ud fra 
               p41280,Date of first in-patient diagnosis - ICD10,440014,https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=41280
               p41281,Date of first in-patient diagnosis - ICD9,20299,https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=41281
               p41282,Date of first operative procedure - OPCS4,440153,https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=41282
-              # Or should I just keep these:
+
 
 
