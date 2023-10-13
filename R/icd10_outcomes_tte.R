7f5977466ff3f83ecab57ec24948ea7fa25ae560
Fie Langmann <fielangmann@gmail.com>
2023-10-13 12:35:32 +0000

    Deleted old code not relevant

--- R/icd10_outcomes_tte.R
+++ R/icd10_outcomes_tte.R
@@ -55,44 +55,6 @@ icd10_subset <- icd10_subset %>%
 
 
 # In lack of better description, I want to do this:
-# if any starts_with("p41270") and ends with "_a[0-259]" includes "K80" or "K81",
-# include any p41280 than ends with the same "_a[0-259]". Variable content not matching
+# if any starts_with("p41270") and ends with "_a[0-258]" includes "K80" or "K81",
+# include any p41280 than ends with the same "_a[0-258]". Variable content not matching
 # these criteria should be changed to "NA"
-
-# Define a vector of suffixes to match
-suffixes_to_match <- paste0("_a", 0:259)
-
-# Apply the logic to update the variables
-# df <- test %>%
-#   rowwise() %>%
-#   mutate(
-#     across(starts_with("p41270"), .fns = ~ {
-#       case_when(
-#         grepl("K80|K81", .) & grepl(paste0("(", paste(suffixes_to_match, collapse = "|"), ")$"), cur_column()) ~ .,
-#         TRUE ~ "NA"
-#       )
-#     }),
-#     across(starts_with("p41280"), .fns = ~ {
-#       case_when(
-#         grepl(paste0("(", paste(suffixes_to_match, collapse = "|"), ")$"), cur_column()) ~ .,
-#         TRUE ~ "NA"
-#       )
-#     })
-#   ) %>%
-#   ungroup()
-
-> update_variable <- function(test, var_prefix, suffixes_to_match) {
-  +     var_names <- names(test)
-  +
-    +     for (suffix in suffixes_to_match) {
-      +         p41270_var_name <- paste0(var_prefix, suffix)
-      +         matching_var_names <- var_names[startsWith(var_names, p41270_var_name)]
-      +         matching_var_names <- matching_var_names[grepl("K80|K81", matching_var_names)]
-      +         test[, matching_var_names] <- ifelse(is.na(test[, matching_var_names]), "NA", test[, matching_var_names])
-      +     }
-  +
-    +     return(test)
-  + }
-
-df <- update_variable(test, "p41270", suffixes_to_match)
-df <- update_variable(test, "p41280", suffixes_to_match)
