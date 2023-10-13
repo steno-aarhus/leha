f45de07c5ed577b7b92daf71dd26810afd6e655c
Fie Langmann <fielangmann@gmail.com>
2023-10-13 12:34:01 +0000

    Updated code for time-to-event diagnoses dates. Error in line 33-34?
    Crashes when running lines 44-45.

--- R/icd10_outcomes_tte.R
+++ R/icd10_outcomes_tte.R
@@ -1,51 +1,57 @@
-# This script will split p41270 (ICD10-codes) into columns and match the date of diagnosis with the
+# This script will split p41270 (ICD10-codes) into columns and match the date of
+# diagnosis with the diagnosis code for that specific data. This is useful when
+# using ICD10 diagnoses as outcomes in time-to-event analyses
+
+
+# On a subset of data -----------------------------------------------------
 # subset of data for overview
 test <- data %>%
-  slice(1:100)
+  slice(55001:60000)
 
-# Split the 'variable into separate variables based on delimiter "|"
-test1 <- test %>%
-  separate_wider_delim(p41270, delim = "|",
-                       names_sep = "var_a", too_few = "debug")
-
-test2 <- test |>
-  separate_wider_delim(p41270,
-                       delim = "|",
-                       names = paste0("p41270var_a", 1:259), too_few = "debug")
-
-# To check if test and test2 matches:
-all.equal(test, test2)
-
-final <- test %>%
+# Split the diagnosis-variable into separate column based on delimiter "|"
+test <- test %>%
   separate_wider_delim(p41270,
                        delim = "|",
                        names = paste0("p41270var_a", 0:258), too_few = "debug")
 
-final %>%  select(matches("_a[0-9]*$"))
+# Transform from wide to long format to match ICD-codes with date of diagnosis
+test %>%  select(matches("_a[0-9]*$"))
 
-test %>%
-  select(matches("p41270|p41280")) %>%
+icd10_subset <- test %>%
+  select(matches("p41270|p41280|id")) %>%
   pivot_longer(cols = matches("_a[0-9]*$"),
                names_to = c(".value", "a"),
-               names_sep = "_") %>%
-  View()
+               names_sep = "_")
 
-# When code works, uncomment below and split variable for all observations
-# data <- data %>%
-#   separate_wider_delim(p41270, delim = "|",
-#                        names_sep = "var_a", too_few = "debug")
+# Remove columns not needed
+remove <- c("p41270_ok", "p41270_pieces", "p41270_remainder")
 
-# Pivot_longer code
-# long_test <- test %>%
-#   pivot_longer(cols = matches("_a[0-259]$"),
-#                names_to = c(".value", "a"),
-#                names_sep = "_")
+icd10_subset <- icd10_subset %>%
+  dplyr::select(-(remove))
 
-# When code works, uncomment below and pivot_longer
-# long_data <- data %>%
-#   pivot_longer(cols = matches("_a[0-259]$"),
-#                names_to = c(".value", "a"),
-#                names_sep = "_")
+# Match variable content across p41270 and p41280 and remove irrelevant dates
+icd10_k80 <- icd10_subset %>%
+  mutate(p41280 = ifelse(str_detect(p41270var, "K80"), as.Date(p41280), "NA")) # This should be coded as.Date but does not?
+
+# When only the relevant dates are left in the data frame, you can transform
+# back to wide and remove irrelevant columns.
+
+# Create a new column for column names in the wide format
+icd10_k80 <- icd10_k80 %>%
+  mutate(new_column = paste("p41280", a, sep = "_"))
+
+# Pivot the data back to wide format
+wide_data <- icd10_k80 %>%
+  pivot_wider(names_from = new_column, values_from = p41280)
+
+
+
+Delete all diagnosis code and only keep the dates? Or can I make a new variable to
+merge the diagnosis code and date to create a variable called date_icd80, date_icd81, etc.?
+
+icd10_subset <- icd10_subset %>%
+  rowwise() %>%
+  mutate(icd10_k80_date = as.Date(unlist(c_across(starts_with("p41280_a")), use.names = FALSE)))
 
 
 # In lack of better description, I want to do this:
