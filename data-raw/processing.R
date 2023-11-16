35c667a3cb49ca1373de1b5c237f58b54e5b7cd7
Fie Langmann <fielangmann@gmail.com>
2023-11-16 11:40:37 +0000

    minor edits

--- data-raw/processing.R
+++ data-raw/processing.R
@@ -3,20 +3,18 @@ library(tidyverse)
 # Processing
 
 # Download saved data
-data <- ukbAid::read_parquet(here::here("data/data.parquet"))
-data <-tibble::as_tibble(data)
+source(here::here("R/data_start.R"))
 
-# DATA MANAGEMENT
-# source data_management.r script
+# Run all data management
 # source(here::here("R/data_management.R"))
 # source diet_data
 # source outcome variables
 
 # Save the changes as parquet and upload to the RAP folder for easy download
 # next time you sign in
-# arrow::write_parquet(data_csv, here("data/ukb_data.parquet"))
+# arrow::write_parquet(data_csv, here("data/data.parquet"))
 
 # Upload to the project RAP folder.
-# ukbAid::upload_data(here("data/ukb_data.parquet"), username = "FieLangmann")
+# ukbAid::upload_data(here("data/data.parquet"), username = "FieLangmann")
 
 
