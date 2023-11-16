ce72dac21a6f81fb8897f7b89a6eec3be1199469
Fie Langmann <fielangmann@gmail.com>
2023-11-16 11:40:10 +0000

    removed surplus code and added code for self-reported disease

--- R/data_management.R
+++ R/data_management.R
@@ -3,37 +3,36 @@ library(dplyr)
 library(magrittr)
 library(tidyr)
 
-
-# Summary overview --------------------------------------------------------
-str(data)
-
-
 # Add ID ------------------------------------------------------------------
-
 data <- data %>%
   mutate(id = 1:n(), .before = everything())
 
 # Remove variables and columns --------------------------------------------
-# Delete variables that were not needed after all -> go to project-
-variables_to_remove <- c("p20160", "p22506", "p2887", "p3436", "p3446", "p6152",
-                         "p20116", "p20117", "p20162", "p22200", "p131668", "p131670",
-                         "p131674", "p131676")
-data1 <- data %>%
-  dplyr::select(-(starts_with(variables_to_remove)| ends_with("_i[0-4]")))
-
-
-
 # Delete follow-up instances for confounder variables
 variables_to_edit <- c("p738", "p1239", "p1538", "p1548", "p3456", "p6150",
                        "p20002","p20107", "p20110", "p20111", "p20161", "p20162",
                        "p21000", "p22040", "p22506", "p22508", "p23104", "p2443",
                        "p2453", "p40000")
-data1 <- data1 %>%
+data <- data %>%
   select(-matches(paste0(variables_to_edit, "_i[1-4]")))
 
 # Recoding variables ------------------------------------------------------
 
+
 # Self-reported non-cancer illness. No diseases coded as 0, liver disease coded as 1, CVD coded as 2, other coded as 3
+# Self-reported diseases should be coded as 0 for those who have not had any relevant
+# diseases, 1 for those who have had CVD but no liver disease, 2 for those who have had
+# liver disease but no CVD and 3 for those who had both liver and CVD.
+data <- data %>%
+  mutate(
+    non_cancer_illness= case_when(
+      str_detect(p20002_i0, "divert") ~ 0,
+      str_detect(p20002_i0, "heart") ~ 1,
+      TRUE ~ NA_integer_  # If none of the conditions match
+    )
+  )
+
+
 Combination of p1239 (smoking status) and p3456 (number of cigarettes currently smoked) to make categories: never smoker; previous smoker; current 1-15; current 15-25; current 25+
   Maybe p22506 (tobacco smoking) + p22508 (amount of tobacco) instead of cigarettes?
 p20161 (pack years of smoking) # is this relevant? Has information on all types
