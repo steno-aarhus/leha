780978763293c2d4e27aa93cf3b5af66337fa36c
Fie Langmann <fielangmann@gmail.com>
2023-11-16 10:02:38 +0000

    added code to source data management file

--- data-raw/processing.R
+++ data-raw/processing.R
@@ -2,12 +2,13 @@ library(tidyverse)
 
 # Processing
 
-# download data
+# Download data
 
 ukb_data <- ukbAid::read_parquet(here::here("data/data.parquet"))
 
 # DATA MANAGEMENT
 # source data_management.r script
+# source(here::here("R/data_management.R"))
 
 # Save the changes as parquet and upload to the RAP folder for easy download
 # next time you sign in
