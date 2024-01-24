d6f31d94b64df0d27fb7b0b5616e21b696f086ac
Fie Langmann <fielangmann@gmail.com>
2024-01-24 14:03:44 +0000

    updated data management and removed those with too few recalls

--- R/data_management.R
+++ R/data_management.R
@@ -3,8 +3,8 @@ library(dplyr)
 library(magrittr)
 library(tidyr)
 
-# Remove uneligible number of recalls ------------
-data <- data %>% 
+# Remove ineligible number of recalls ------------
+data <- data %>%
 subset(p20077>=2)
 
 # Add ID ------------------------------------------------------------------
@@ -13,15 +13,15 @@ data <- data %>%
 
 # Remove variables and columns --------------------------------------------
 # Delete follow-up instances for confounder variables
-variables_to_edit <- c("p738", "p1239", "p1538", "p1548", "p3456", "p6150",
+variables_to_edit <- c("p738", "p2443", "p2453", "p3456", "p6150",
                        "p20002","p20107", "p20110", "p20111", "p20161", "p20162",
                        "p21000", "p22040", "p22506", "p22508", "p23104", "p2443",
                        "p2453", "p40000")
 data <- data %>%
   select(-matches(paste0(variables_to_edit, "_i[1-4]")))
 
-# Recoding variables ------------------------------------------------------
 
+# Recoding variables ------------------------------------------------------
 
 # Self-reported non-cancer illness. No diseases coded as 0, liver disease coded as 1, CVD coded as 2, other coded as 3
 # Self-reported diseases should be coded as 0 for those who have not had any relevant
@@ -36,13 +36,60 @@ data <- data %>%
     )
   )
 
+# Smoking status categorized as never, former, current 1-15, current 15-25, current 25+, and no answer
+data <- data %>%
+  mutate(
+    smoking = case_when(
+      str_detect(p20116_i0, "Never") ~ 0,
+      str_detect(p20116_i0, "Previous") ~ 1,
+      str_detect(p20116_i0, "Current") & p3456_i0 < 15 ~ 2,
+      str_detect(p20116_i0, "Current") & p3456_i0 >= 15 & p3456_i0 <= 25 ~ 3,
+      str_detect(p20116_i0, "Current") & p3456_i0 > 25 ~ 4,
+      str_detect(p20116_i0, "answer") ~ 5,
+      TRUE ~ NA_real_  # Handling cases not covered by the conditions
+    )
+  )
 
-Combination of p1239 (smoking status) and p3456 (number of cigarettes currently smoked) to make categories: never smoker; previous smoker; current 1-15; current 15-25; current 25+
-  Maybe p22506 (tobacco smoking) + p22508 (amount of tobacco) instead of cigarettes?
-p20161 (pack years of smoking) # is this relevant? Has information on all types
-# of smoking.
+p20107,Illnesses of father,488041,https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=20107
+p20110,Illnesses of mother,492917,https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=20110
+p20111
 
-Smoking: never, former, current (pack years)
+# Illness in closest family
+# Father
+data <- data %>%
+  mutate(
+    father_illness= case_when(
+      str_detect(p20107_i0, "Diabetes") ~ 0,
+      str_detect(p20107_i0, "High blood pressure") ~ 1,
+      str_detect(p20107_i0, "Stroke") ~ 2,
+      str_detect(p20107_i0, "Heart disease") ~ 3,
+      TRUE ~ NA_integer_  # If none of the conditions match
+    )
+  )
+
+# Mother
+data <- data %>%
+  mutate(
+    mother_illness= case_when(
+      str_detect(p20110_i0, "Diabetes") ~ 0,
+      str_detect(p20110_i0, "High blood pressure") ~ 1,
+      str_detect(p20110_i0, "Stroke") ~ 2,
+      str_detect(p20110_i0, "Heart disease") ~ 3,
+      TRUE ~ NA_integer_  # If none of the conditions match
+    )
+  )
+
+# Siblings
+data <- data %>%
+  mutate(
+    sibling_illness= case_when(
+      str_detect(p20111_i0, "divert") ~ 0,
+      str_detect(p20111_i0, "heart") ~ 1,
+      str_detect(p20111_i0, "Stroke") ~ 2,
+      str_detect(p20111_i0, "Heart disease") ~ 3,
+      TRUE ~ NA_integer_  # If none of the conditions match
+    )
+  )
 
 
 # Convert data to numerics
@@ -56,6 +103,19 @@ filter(data, is.na(temp) & !is.na(varname)) %>%
 
 
 # Now run diet-data-scripts (probably the sort variables.R scripts)
+
+
+
+
+
+
+# Remove old variables used to make new -----------------------------------
+
+variables_to_remove <- c("p3456", "p20107", "p20110", "p20111")
+
+# data <- data %>%
+#   select(-any_of(variables_to_remove))
+# the above does not work?
 
 
 # New column names --------------------------------------------------------
