dd5d004dc32d51ad9a5893ed4e775d455f724273
Fie Langmann <fielangmann@gmail.com>
2024-02-08 15:06:37 +0000

    updated data management script

--- R/data_management.R
+++ R/data_management.R
@@ -15,7 +15,7 @@ data <- data %>%
 # Delete follow-up instances for confounder variables
 variables_to_edit <- c("p738", "p2443", "p2453", "p3456", "p6150",
                        "p20002","p20107", "p20110", "p20111", "p20161", "p20162",
-                       "p21000", "p22040", "p22506", "p22508", "p23104", "p2443",
+                       "p21000", "p22040", "p22506", "p23104", "p2443",
                        "p2453", "p40000")
 data <- data %>%
   select(-matches(paste0(variables_to_edit, "_i[1-4]")))
@@ -23,24 +23,75 @@ data <- data %>%
 
 # Recoding covariables (not foods) ------------------------------------------------------
 
-sex
+# sex
+data <- data %>%
+  mutate(sex = case_when(
+    str_detect(p31, "Fem") ~ 0, #female
+    str_detect(p31, "Male") ~ 1, #male
+    TRUE ~ NA_integer_  # If none of the conditions match
+  )
+)
 
+# strata of age at recruitment
+data <- data %>%
+  mutate(age_strata = case_when(
+    p21022 < 45 ~ 0,
+    p21022 >= 45 & p21022 <= 49 ~ 1,
+    p21022 >= 50 & p21022 <= 54 ~ 2,
+    p21022 >= 55 & p21022 <= 59 ~ 3,
+    p21022 >= 60 & p21022 <= 64 ~ 4,
+    p21022 >= 65 ~ 5
+  )
+)
 
-age
-strata of age at recruitment
-(\<45, 45-49, 50-54, 55-59, 60-64, ≤65 years)
-ethnic group
+# ethnic background
+data <- data %>%
+  mutate(ethnicity = case_when(
+    p21000_i0 == "White" | p21000_i0 == "British" | p21000_i0 == "Irish" | p21000_i0 == "Any other white background" ~ 0,
+    p21000_i0 == "Mixed" | p21000_i0 == "White and Black Caribbean" |p21000_i0 == "White and Black African" | p21000_i0 == "White and Asian" | p21000_i0 == "Any other mixed background" ~ 1,
+    p21000_i0 == "Asian or Asian British" | p21000_i0 =="Indian" | p21000_i0 == "Pakistani" | p21000_i0 == "Bangladeshi" | p21000_i0 == "Any other Asian background" ~ 2,
+    p21000_i0 == "Black or Black British" | p21000_i0 == "Caribbean" | p21000_i0 == "African" | p21000_i0 == "Any other Black background" ~ 3,
+    p21000_i0 == "Chinese" ~ 4,
+    p21000_i0 == "Other ethnic group" ~ 5,
+    p21000_i0 == "Do not know" | p21000_i0 == "Prefer not to answer" | str_detect(p21000_i0, "NA") ~ 6
+  )
+)
 
-townsend
+# townsend deprivation index
+data <- data %>%
+  mutate(townsend_deprivation_index = p22189)
 
-educational level
+# educational level
+data <- data %>%
+  mutate(education_short = as.character(str_sub(p6138_i0, start = 1, end = 28)),
+         education = case_when(
+    education_short == "College or University degree" ~ "High",
+    education_short == "A levels/AS levels or equiva" ~ "Intermediate",
+    education_short == "O levels/GCSEs or equivalent" ~ "Intermediate",
+    education_short == "CSEs or equivalent|NVQ or HN" ~ "Low",
+    education_short == "CSEs or equivalent|Other pro" ~ "Low",
+    education_short == "CSEs or equivalent" ~ "Low",
+    education_short == "NVQ or HND or HNC or equival" ~ "Low",
+    education_short == "Other professional qualifica" ~ "Low",
+    education_short == "None of the above" ~ "Low"
+  )
+)
 
-Uk regions
+# cohabitation (living with a wife or partner (yes, no))
+p6141
 
-cohabitation
+# physical activity
+# Check for normal distribution:
+library(ggplot2)
+data %>% ggplot(aes(x=p22040_i0))+
+  geom_histogram(aes(y=..density..),bins=10)+
+  stat_function(fun=dnorm,
+                args=list(mean=mean(data$p22040_i0),sd=sd(data$p22040_i0)))
 
-physical activity
-
+# not normally distributed, categories?
+data <- data %>%
+  mutate()
+p22040_i0,Summed MET minutes per week for all activity,402303,https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=22040
 
 # Smoking status categorized as never, former, current 1-15, current 15-25, current 25+, and no answer
 data <- data %>%
@@ -61,16 +112,20 @@ data <- data %>%
 data <- data %>%
   mutate(
     non_cancer_illness= case_when(
-      str_detect(p20002_i0, "hypert") ~ 0, # hypertension
-      str_detect(p20002_i0, "myocardial") ~ 1, # MI
-      str_detect(p20002_i0, "stroke") | (p20002_i0, "ischaemic") | (p20002_i0, "haemorrhage")  ~ 2, # Stroke
+      str_detect(p20002_i0, "hypert") | (p6150_i0, "High") ~ 0, # hypertension
+      str_detect(p20002_i0, "myocardial") | (p6150_i0, "Heart") ~ 1, # MI
+      str_detect(p20002_i0, "stroke") | (p20002_i0, "ischaemic") | (p20002_i0, "haemorrhage") | (p6150_i0, "Stroke")~ 2, # Stroke
       str_detect(p20002_i0, "cholesterol") ~ 3, # high cholesterol
       str_detect(p20002_i0, "cholangitis") | (p20002_i0, "cholelithiasis") | (p20002_i0, "cholecyst") | (p20002_i0, "primary biliary cirrhosis") ~ 4, #gallbladder problems
       str_detect(p20002_i0, "alcoholic cirrhosis") ~ 5, #alcohol liver disease
+      str_detect(p6150_i0, "Angina") ~ 6, # angina pectoris
       TRUE ~ NA_integer_  # If none of the conditions match
     )
   )
 
+diabetes doctor str_detect(p2443_i0, )
+
+p2453,Cancer diagnosed by doctor,501498,https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=2453
 
 # Illness in closest family - skal den her være med?
 # Father
@@ -113,6 +168,9 @@ data <- data %>%
 # BMI cut-off 30
 
 
+#energy intake
+energy = rowMeans(dplyr::across(dplyr::starts_with("p26002")), na.rm = TRUE) * 0.239,
+
 # Convert data to numerics
 # TO check if a variable is character, run this code
 table(grepl(" ", data$varname)) #table of all variables with spaces in the cell content;
@@ -132,7 +190,8 @@ filter(data, is.na(temp) & !is.na(varname)) %>%
 
 # Remove old variables used to make new -----------------------------------
 
-variables_to_remove <- c("p3456", "p20107", "p20110", "p20111")
+variables_to_remove <- c("p3456", "p20107", "p20110", "p20111", "p31",
+                         "p21000_i0", "p22189", "p20002". "p6138")
 
 # data <- data %>%
 #   select(-any_of(variables_to_remove))
