f12b13ddcf9a75e7d4005ef23b2e5c5d57c371ef
Fie Langmann <fielangmann@gmail.com>
2024-02-16 09:53:26 +0000

    Minor edits

--- data-raw/processing.R
+++ data-raw/processing.R
@@ -3,7 +3,7 @@ library(tidyverse)
 # Processing
 
 # Download saved data
-source(here::here("R/data_start.R"))
+source(here::here("R/1_data_start.R"))
 
 # Run all data management
 # source(here::here("R/data_management.R"))
@@ -12,9 +12,10 @@ source(here::here("R/data_start.R"))
 
 # Save the changes as parquet and upload to the RAP folder for easy download
 # next time you sign in
-# arrow::write_parquet(data_csv, here("data/data.parquet"))
+arrow::write_parquet(sorted_data, here("data/sorted_data.parquet"))
 
 # Upload to the project RAP folder.
-# ukbAid::upload_data(here("data/data.parquet"), username = "FieLangmann")
+ukbAid::upload_data(here("data/sorted_data.parquet"), username = "FieLangmann")
 
-
+# Go to UKB RAP and manually change name of uploaded .parquet to sorted_data.parquet
+# Now you can run your analyses with sorted_data as data frame
