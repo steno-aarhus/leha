a44f7221f29176ba17deaf00449ab3c34f0c816a
Fie Langmann <fielangmann@gmail.com>
2024-02-16 18:28:56 +0000

    updated processing.R

--- data-raw/processing.R
+++ data-raw/processing.R
@@ -1,14 +1,12 @@
+# Processing
+# Load packages -----------------------------------------------------------
 library(tidyverse)
 
-# Processing
-
-# Download saved data
-source(here::here("R/1_data_start.R"))
-
-# Run all data management
-# source(here::here("R/data_management.R"))
-# source diet_data
-# source outcome variables
+# Load saved data:
+# source(here::here("R/1_data_start.R")) #loads data before data management
+# source(here::here("R/2_data_management.R")) #loads data and runs data management
+# source(here::here("R/3_diet_data.R")) #loads sorted_data and runs diet code
+# source(here::here("R/4_icd10_outcomes_tte.R") #loads sorted_data and runs outcome code
 
 # Save the changes as parquet and upload to the RAP folder for easy download
 # next time you sign in
