e8c7b14e687b403b1a6ad571011901c436e7c956
Fie Langmann <fielangmann@gmail.com>
2024-02-17 11:19:32 +0000

    matched ICD10 codes with dates for NAFLD and NASH

--- R/4_icd10_outcomes_tte.R
+++ R/4_icd10_outcomes_tte.R
@@ -6,48 +6,12 @@
 library(tidyverse)
 library(magrittr)
 library(dplyr)
+library(here)
 
-# Load sorted_data ---------------------------------------------------------------
+# Load data ---------------------------------------------------------------
 sorted_data <- ukbAid::read_parquet(here("data/sorted_data.parquet"))
 # # Converting the dataset into a tibble to work with for analyses
 sorted_data <-tibble::as_tibble(sorted_data)
-
-# On a subset of data -----------------------------------------------------
-# subset of data for overview
-test <- sorted_data %>%
-  slice(115000:120000) %>%
-  select(starts_with("p41270"), starts_with("p41280"), "id") %>%
-# Split the diagnosis-variable into separate column based on delimiter "|"
-  separate_wider_delim(p41270,
-                       delim = "|",
-                       names = paste0("p41270var_a", 0:258), too_few = "debug")
-
-# Transform from wide to long format to match ICD-codes with date of diagnosis
-icd10_subset <- test %>%
-  select(matches("p41270|p41280|id")) %>%
-  pivot_longer(cols = matches("_a[0-9]*$"),
-               names_to = c(".value", "a"),
-               names_sep = "_")
-
-# Filter for relevant rows and convert dates
-icd10_nafld <- icd10_subset %>%
-  mutate(icd10_nafld_date = ifelse(str_detect(p41270var, "K76.0"),
-                                   as.character(c_across(starts_with("p41280"))),
-                                   NA),
-         icd10_nafld_date = as.Date(icd10_nafld_date, format = "%Y-%m-%d"))
-
-# Now you can merge the 'icd10_nafld_date' back into the original wide-format data frame
-# Aggregate icd10_nafld by participant ID to match the number of rows in test
-aggregated_icd10_nafld <- icd10_nafld %>%
-  group_by(id) %>%
-  summarise(icd10_nafld_date = first(icd10_nafld_date))  # Or use another summarization method
-
-sum(!is.na(aggregated_icd10_nafld$icd10_nafld_date))
-
-# Now you can merge the 'aggregated_icd10_nafld' back into the 'test' data frame
-merged_data <- test %>%
-  left_join(aggregated_icd10_nafld, by = "id")
-
 
 # Full data frame ---------------------------------------------------------
 # Split the diagnosis-variable into separate columns based on delimiter "|"
@@ -64,22 +28,51 @@ icd10_subset <- icd %>%
                names_to = c(".value", "a"),
                names_sep = "_")
 
-# Filter for relevant rows and convert dates
+# Defining dates of NAFLD
 icd10_nafld <- icd10_subset %>%
   mutate(icd10_nafld_date = ifelse(str_detect(p41270var, "K76.0"),
                                    as.character(c_across(starts_with("p41280"))),
                                    NA),
          icd10_nafld_date = as.Date(icd10_nafld_date, format = "%Y-%m-%d"))
 
-# Now you can merge the 'icd10_nafld_date' back into the original wide-format data frame
-# Aggregate icd10_nafld by participant ID to match the number of rows in test
-aggregated_icd10_nafld <- icd10_nafld %>%
+first_non_na_nafld <- icd10_nafld %>%
+  filter(!is.na(icd10_nafld_date)) %>%
   group_by(id) %>%
-  summarise(icd10_nafld_date = first(icd10_nafld_date))  # Or use another summarization method
+  slice(1) %>%
+  ungroup()
 
-# Now you can merge the 'aggregated_icd10_nafld' back into the 'test' data frame
-merged_data <- sorted_data %>%
-  left_join(aggregated_icd10_nafld, by = "id")
+sorted_data <- sorted_data %>%
+  left_join(first_non_na_nafld %>% select(id, icd10_nafld_date), by = "id")
+
+# Defining dates of NASH
+icd10_nash <- icd10_subset %>%
+  mutate(icd10_nash_date = ifelse(str_detect(p41270var, "K75.8"),
+                                   as.character(c_across(starts_with("p41280"))),
+                                   NA),
+         icd10_nash_date = as.Date(icd10_nash_date, format = "%Y-%m-%d"))
+
+first_non_na_nash <- icd10_nash %>%
+  filter(!is.na(icd10_nash_date)) %>%
+  group_by(id) %>%
+  slice(1) %>%
+  ungroup()
+
+sorted_data <- sorted_data %>%
+  left_join(first_non_na_nash %>% select(id, icd10_nash_date), by = "id")
+
+
+# Save data ---------------------------------------------------------------
+arrow::write_parquet(sorted_data, here("data/sorted_data.parquet"))
+
+
+# Delete old ICD10 diagnosis and dates ------------------------------------
+delete <- c("p41280", "p41270")
+sorted_data <- sorted_data %>%
+  select(-matches(paste0(delete)))
+
+# Save data ---------------------------------------------------------------
+arrow::write_parquet(sorted_data, here("data/sorted_data.parquet"))
+
 
 # Set cut-off date for follow-up ------------------------------------------
 # Estimated last follow-up date for ICD10 codes (stagnation of diagnoses)
