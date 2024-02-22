7216a3c899d56b60b3c21bafbc10c636ec45e3ac
Fie Langmann <fielangmann@gmail.com>
2024-02-22 19:33:37 +0000

    updated matrix syntax for table1

--- R/L3. Descriptives.R
+++ R/L3. Descriptives.R
@@ -1,24 +1,77 @@
 #3. Descriptive
+
+# Load packages -----------------------------------------------------------
+
+
 install.packages(glue)
 library(glue)
 library(scales)
 
 
-#Table 1
-table<-matrix(NA,nrow=10, ncol=4) #fit rows and table numbers to content.
+# Load data ---------------------------------------------------------------
+targets::tar_make()
+# Restart session
+source(here::here("R/1_data_start.R"))
+
+
+
+# Table 1 -----------------------------------------------------------------
+# Create a matrix to feed results into
+table <- matrix(NA,nrow=20, ncol=3) #fit rows and table numbers to content.
 #run analyses and get estimate (x), lowerCI, upperCI or percentiles
 x<-number(x, accuracy=0.01) #will specify a new object X from the last run code with 2dp
-lower<-number(lowCI, accuracy=0.01) #will specify lower CI with 2dp
-upper<-number(upperCI, accuracy=0.01) #will specify upper CI with 2dp
+lower<-number(10p, accuracy=0.01) #will specify 10 percentile with 2dp
+upper<-number(90p, accuracy=0.01) #will specify 90 percentile with 2dp
+percent <- number(x/ nrows() *100, accuracy = 0.01) # will specify percentage with 2dp
 5p<-number(x, accuracy=0.01)
 95p<-number(x, accuracy=0.01)
 
-table[1,1]<-"Column1heading"
-table[1,2]<-"column1content1"
-table[1,3]<-"column1content2"
-table[2,1]glue("{x} ({lower};{upper})") #this will glue estimates defined earlier into my matrix-table. Should be done for all content before next code
+table[1,1]<-"Characteristics"
+table[1,2]<-"All participants"
+table[1,3]<-"Individuals with NAFLD or NASH"
+table[2,1] <-"Sex, female"
+table[2,2]glue("{x}, {percent}")
+table[2,3]glue("{x}, {percent}")
+table[3,1] <- "Age at recruitment, years"
+table[3,2]glue("{x} ({lower};{upper})")
+table[3,3]glue("{x} ({lower};{upper})")
+table[4,1] <-
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+table[4,1]glue("{x} ({lower};{upper})")
+
+glue("{x} ({lower};{upper})") #this will glue estimates defined earlier into my matrix-table. Should be done for all content before next code
 table<-data.frame(table)
-kable(table,"oprionshere")
+kable(table,"optionshere")
 
 
-https://www.danieldsjoberg.com/gtsummary/ --> kan være et godt alternativ også.
+
+
+
