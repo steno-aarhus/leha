e54c62b2105e306296e80abd80ac7c6585433f3f
Fie Langmann <fielangmann@gmail.com>
2024-02-28 13:52:28 +0000

    minor edits

--- R/5_descriptives.R
+++ R/5_descriptives.R
@@ -23,12 +23,6 @@ source(here::here("R/1_data_start.R"))
 
 
 # Table 1 -----------------------------------------------------------------
-data <- data %>%
-  mutate(nafld = case_when(
-    !is.na(icd10_nafld_date) | !is.na(icd10_nash_date) |
-      !is.na(icd9_nafld_date) | !is.na(icd9_nash_date) ~ 1,
-    TRUE ~ 0))
-
 table1 <- data %>%
   select(nafld, age, sex, yearly_income, education, deprivation, cohabitation, ethnicity, physical_activity, smoking, alcohol_daily, region, bmi30, diabetes, non_cancer_illness, cancer, family_illness) %>%
   tbl_summary(by = nafld,
