e8647514b14b387262d02fe656638a4b16d40379
Fie Langmann <fielangmann@gmail.com>
2024-05-06 09:09:12 +0000

    updated code for model assumption 2 and 4

--- R/9_pseudo_assumptions.R
+++ R/9_pseudo_assumptions.R
@@ -1,20 +1,4 @@
 # This script tests the model assumptions for Pseudo Observational analyses
-
-# FRA CHRISTIAN
-# Det kunne være fint hvis du har relevante tidsvariabler (alder, kalenderdato)
-# samt alle apriori-specificerede kovariater og får sat op til at lave cox
-# regressioner på din ønskede tidsskala (alder?) med dit outcome of interest.
-# Herefter kan første skridt være at modellere hhv. baseline alder og baseline
-# date og risikoen for dit event med cox regression med fx 4 knudepunkter med
-# alder som tidsakse.
-#
-# Det kan også være en fordel at have en status-variabel som definerer om de har
-# fået event, er døde, tabt for follow-up eller administrativt censorerede.
-# Tidsrammen defineres først når modelantagelserne er vurderes opfyldt hvorefter
-# antagelserne testes forfra inden for tidsrammen. Man skal også sikre sig, at der
-# er tilstrækkeligt mange deltagere at risk inden for hvert temporært substrata.
-
-
 # Load packages
 # Load packages
 library(tidyverse)
@@ -55,6 +39,14 @@ data <- data %>%
     status != "censored" ~ "no"
   ))
 
+# Baseline age
+data <- data %>%
+  mutate(baseline_age = (date_filled - date_birth)/365.25)
+# age strata
+data <- data %>%
+  mutate(base_age_strata = ntile(baseline_age, 5))
+
+
 # Assumption 2: Event risk is independent of censoring (The independent censoring assumption)
 # This assumption can only be examined with regard to administrative censoring
 # Test: Investigate the association between baseline date and event risk within strata of baseline age using Cox
@@ -67,16 +59,23 @@ cox <- tidy(cox, exponentiate = TRUE, conf.int = TRUE)
 # association between censoring and outcome
 
 # within age strata
-cox <- data %>% filter(base_age_strata==3) %>% coxph(Surv(survival_time, nafld == 1) ~ date_filled,
+cox1 <- data %>% filter(base_age_strata==3) %>% coxph(Surv(survival_time, nafld == 1) ~ date_filled,
                                               data = ., ties='breslow')
+cox1 <- tidy(cox1, exponentiate = TRUE, conf.int = TRUE)
+# Aldersstrata er væsentlige og bør tilføjes til Pseudo koden nedenfor for at fjerne
+# afhængighed mellem event risk og censoring.
 
+##### PSEUDO - justeret for alle covariate med strata af alder i modellen
+data <- data %>%
+  mutate(
+    legumes80 = legume_weekly / 80,
+    meats80 = meats_weekly / 80,
+    poultry80 = poultry_weekly / 80,
+    fish80 = fish_weekly / 80
+  )
 
-# Aldersstrata er væsentlige og bør tilføjes til Pseudo koden nedenfor:
-# model.censoring = "stratified",
-# formula.censoring = ~ sex, <- skal være aldersstrata
-
-Censorering som udfald i antagelse 4
-fit_meat <- eventglm::cumincglm(Surv(time, nafld == 1) ~
+# hvad skal eventstatus være? censurering eller sygdom?
+fit_meat2 <- eventglm::cumincglm(Surv(time, nafld == 1) ~
                                   # removing meat
                                   legumes80 + poultry80 + fish80+
                                   #other food components
@@ -86,29 +85,15 @@ fit_meat <- eventglm::cumincglm(Surv(time, nafld == 1) ~
                                   non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
                                   sauce_weekly + weight_weekly +
                                   #other variables
-                                  age + region + sex +
+                                  baseline_age + region + sex +
                                   alcohol_weekly + ethnicity + deprivation + education +
                                   cohabitation + physical_activity + smoking +
                                   related_disease + disease_family + yearly_income,
-                                time = 10, data = data)
+                                time = 10, cause = "censored",
+                                model.censoring = "stratified",
+                                formula.censoring = ~ base_age_strata, data = data) #skal det være strataet jeg har lavet, eller skal det bare være den kontinuære variabel "age"?
 
-fit_meat <- tidy(fit_meat, exponentiate = FALSE, conf.int = TRUE, digits = 2)
-
-
-
-
-
-
-
-
-# Start of follow-up = date_filled (Calendar date for last filled questionnaire)
-# Baseline age
-data <- data %>%
-  mutate(baseline_age = (date_filled - date_birth)/365.25)
-# age strata
-data <- data %>%
-  mutate(base_age_strata = ntile(baseline_age, 5))
-
+fit_meat2 <- tidy(fit_meat2, exponentiate = FALSE, conf.int = TRUE)
 
 
 # Linear association between baseline date and event risk stratified on age
@@ -128,23 +113,48 @@ model0 <- lm(nafld ~ date_filled, data = age0)
 # relevant time axis. The outcome can be defined as a composite of censoring due to emigration and
 # administrative censoring.
 
+# association between covariates and censoring
+# age
+assump4_age <- coxph(Surv(survival_time, censored == "yes") ~ base_age_strata, # eller baseline_age? og within strata?
+              data = data, ties = 'breslow')
+assump4_age <- tidy(assump4_age, exponentiate = TRUE, conf.int = TRUE)
+# within each age strata
+cox_age <- function(df) {
+  cox_model <- coxph(Surv(survival_time, censored == "yes") ~ baseline_age, data = df, ties = 'breslow')
+  tidy(cox_model, exponentiate = TRUE, conf.int = TRUE)
+}
+
+# Group by base_age_strata, fit Cox model, and tidy the results for each group
+cox_results <- data %>%
+  group_by(base_age_strata) %>%
+  nest() %>%
+  mutate(cox_results = map(data, cox_age)) %>%
+  unnest(cox_results)
+# View the results
+print(cox_results)
+
+# sex
+assump4_sex <- coxph(Surv(survival_time, censored == "yes") ~ sex,
+                     data = data, ties = 'breslow')
+assump4_sex <- tidy(assump4_sex, exponentiate = TRUE, conf.int = TRUE)
+# Within age strata
+cox_sex <- function(df) {
+  cox_model <- coxph(Surv(survival_time, censored == "yes") ~ sex, data = df, ties = 'breslow')
+  tidy(cox_model, exponentiate = TRUE, conf.int = TRUE)
+}
+
+# Group by base_age_strata, fit Cox model, and tidy the results for each group
+cox_results <- data %>%
+  group_by(base_age_strata) %>%
+  nest() %>%
+  mutate(cox_results = map(data, cox_sex)) %>%
+  unnest(cox_results)
+# View the results
+print(cox_results)
+
+
+
 # Covariates (måske ikke kostvariable?)
-# cereal_refined_weekly
-# whole_grain_weekly
-# mixed_dish_weekly
-# dairy_weekly
-# fats_weekly
-# fruit_weekly
-# nut_weekly
-# veggie_weekly
-# potato_weekly
-# egg_weekly
-# meat_sub_weekly
-# non_alc_beverage_weekly
-# alc_beverage_weekly
-# snack_weekly
-# sauce_weekly
-# weight_weekly
 # age
 # region
 # sex
@@ -162,8 +172,20 @@ model0 <- lm(nafld ~ date_filled, data = age0)
 
 
 # Pseudo model
+fit_meat <- eventglm::cumincglm(Surv(time, nafld == 1) ~
+                                  # removing meat
+                                  legumes80 + poultry80 + fish80+
+                                  #other food components
+                                  cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
+                                  dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
+                                  veggie_weekly + potato_weekly + egg_weekly +
+                                  non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
+                                  sauce_weekly + weight_weekly +
+                                  #other variables
+                                  age + region + sex +
+                                  alcohol_weekly + ethnicity + deprivation + education +
+                                  cohabitation + physical_activity + smoking +
+                                  related_disease + disease_family + yearly_income,
+                                time = 10, data = data)
 
-
-# meats
-
-
+fit_meat <- tidy(fit_meat, exponentiate = FALSE, conf.int = TRUE, digits = 2)
