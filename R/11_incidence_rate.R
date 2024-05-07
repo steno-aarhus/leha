592c79d07ebc20056e46150de58dd70e8e8791ca
Fie Langmann <fielangmann@gmail.com>
2024-05-07 07:34:34 +0000

    new script for estimating incidence rates

---
+++ R/11_incidence_rate.R
@@ -0,0 +1,10 @@
+# 11. Incidence rates
+
+# Overall incidence rate
+
+
+# Incidence rate difference when replacing meat with pulses
+
+# Incidence rate difference when replacing poultry with pulses
+
+# Incidence rate difference when replacing fish with pulses
