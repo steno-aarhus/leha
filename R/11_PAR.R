f67db6154dcd901c9922d7e0e31677a2dc099548
Fie Langmann <fielangmann@gmail.com>
2024-05-10 07:36:00 +0000

    minor edits

--- R/11_PAR.R
+++ R/11_PAR.R
@@ -13,6 +13,8 @@ library(ggsurvfit)
 library(ggplot2)
 library(dplyr)
 library(here)
+library(graphPAF)
+
 
 # load data
 # Load data --------------------------------------------------------
@@ -50,46 +52,6 @@ meat_model2 <- coxph(
   data = data, ties = "breslow"
 )
 
-# poultry
-poultry_model2 <- coxph(
-  Surv(survival_time, nafld == 1) ~
-    # removing meat
-    legumes80 + meats80 + fish80 +
-    # other food components
-    cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-    dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-    veggie_weekly + potato_weekly + egg_weekly +
-    non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-    sauce_weekly + weight_weekly +
-    # other variables
-    sex +
-    alcohol_weekly + ethnicity + deprivation + education +
-    cohabitation + physical_activity + smoking +
-    related_disease + disease_family + yearly_income +
-    strata(region, age_strata),
-  data = data, ties = "breslow"
-)
-
-# fish
-fish_model2 <- coxph(
-  Surv(survival_time, nafld == 1) ~
-    # removing meat
-    legumes80 + meats80 + poultry80 +
-    # other food components
-    cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-    dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-    veggie_weekly + potato_weekly + egg_weekly +
-    non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-    sauce_weekly + weight_weekly +
-    # other variables
-    sex +
-    alcohol_weekly + ethnicity + deprivation + education +
-    cohabitation + physical_activity + smoking +
-    related_disease + disease_family + yearly_income +
-    strata(region, age_strata),
-  data = data, ties = "breslow"
-)
-
 
 # PAF
 paf_meat <- PAF_calc_continuous(model=meat_model2, riskfactor_vec = c("legumes80"), # how to add meat?
@@ -97,3 +59,45 @@ paf_meat <- PAF_calc_continuous(model=meat_model2, riskfactor_vec = c("legumes80
                                 data = data, ci = TRUE, boot_rep = 50, verbose=TRUE,
                                 ci_level = 0.95, ci_type = c("norm"),
                                 t_vector=65) #how is time added in model? from baseline or from birth?
+
+
+
+# # poultry
+# poultry_model2 <- coxph(
+#   Surv(survival_time, nafld == 1) ~
+#     # removing meat
+#     legumes80 + meats80 + fish80 +
+#     # other food components
+#     cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
+#     dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
+#     veggie_weekly + potato_weekly + egg_weekly +
+#     non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
+#     sauce_weekly + weight_weekly +
+#     # other variables
+#     sex +
+#     alcohol_weekly + ethnicity + deprivation + education +
+#     cohabitation + physical_activity + smoking +
+#     related_disease + disease_family + yearly_income +
+#     strata(region, age_strata),
+#   data = data, ties = "breslow"
+# )
+#
+# # fish
+# fish_model2 <- coxph(
+#   Surv(survival_time, nafld == 1) ~
+#     # removing meat
+#     legumes80 + meats80 + poultry80 +
+#     # other food components
+#     cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
+#     dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
+#     veggie_weekly + potato_weekly + egg_weekly +
+#     non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
+#     sauce_weekly + weight_weekly +
+#     # other variables
+#     sex +
+#     alcohol_weekly + ethnicity + deprivation + education +
+#     cohabitation + physical_activity + smoking +
+#     related_disease + disease_family + yearly_income +
+#     strata(region, age_strata),
+#   data = data, ties = "breslow"
+# )
