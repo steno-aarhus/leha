d68f6a6557fadff942414c51dacda8e29b0efc53
Fie Langmann <fielangmann@gmail.com>
2024-06-04 09:45:17 +0000

    Added baseline characteristics table across legume intake groups

--- R/5_descriptives.R
+++ R/5_descriptives.R
@@ -9,10 +9,8 @@ library(splines)
 library(gtsummary)
 library(flextable)
 
-# Load data ---------------------------------------------------------------
-targets::tar_make()
-# Restart session
-source(here::here("R/1_data_start.R"))
+# Load data
+data <- read_csv(here("data/data.csv"))
 
 # Table 1 -----------------------------------------------------------------
 table1 <- data %>%
@@ -30,24 +28,46 @@ table1 <- data %>%
 flextable::save_as_html(table1, path = here("doc", "table1.html"))
 flextable::save_as_docx(table1, path = here("doc", "table1.docx"))
 
-# food group intakes
-table_foods <- data %>%
-  select(nafld, legume_weekly, meats_weekly, poultry_weekly, fish_weekly,
+# baseline characteristics across legume intakes
+# subset to only include consumers
+consumers <- data %>%
+  subset(legume_weekly != 0)
+# define and print the tertile of legume consumption among consumers
+cons <- consumers %>%
+  mutate(legume_tert = ntile(legume_weekly, 3))
+tertile_breaks <- quantile(cons$legume_weekly, probs = seq(0, 1, 1/3))
+print(tertile_breaks)
+
+# include the tertiles in the dataframe
+data <- data %>% mutate(
+  legume_groups = case_when(legume_weekly == 0 ~ 0,
+                            legume_weekly >0 & legume_weekly < 163.3 ~ 1,
+                            legume_weekly >= 163.3 & legumes_weekly < 358.75 ~ 2,
+                            legume_weekly >= 358.75 ~ 3
+  )
+)
+
+# making the descriptive table
+suppl_table2 <- data %>%
+  select(legume_groups, legume_weekly, nafld, age, sex, yearly_income, education,
+         deprivation, cohabitation, ethnicity, physical_activity, smoking,
+         alcohol_weekly, region, bmi30, related_disease, cancer, disease_family,
+         meats_weekly, poultry_weekly, fish_weekly,
          cereal_refined_weekly, whole_grain_weekly, mixed_dish_weekly,
          dairy_weekly, fats_weekly, fruit_weekly, nut_weekly, veggie_weekly,
          potato_weekly, egg_weekly, non_alc_beverage_weekly,
          alc_beverage_weekly, snack_weekly, sauce_weekly, weight_weekly) %>%
-  tbl_summary(by = nafld,
+  tbl_summary(by = legume_groups,
               statistic = list(all_continuous() ~  "{median} ({p10}, {p90})",
                                all_categorical() ~ "{n} ({p}%)"),
               digits = all_continuous() ~ 1,
               missing_text = "n missing") %>%
   add_overall() %>%
   bold_labels() %>%
-  modify_caption("Table X. Median and 10-90 percentiles of weekly intake of food groups included in substitution analyses") %>%
+  modify_caption("Supplementary Table 2. Baseline characteristics across consumption of legumes in the UK Biobank cohort") %>%
   as_flex_table()
 
-flextable::save_as_html(table_foods, path = here("doc", "table_foods.html"))
+flextable::save_as_html(suppl_table2, path = here("doc", "suppl_table2.html"))
 
 
 
