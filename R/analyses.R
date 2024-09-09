b2ea9109d8d30ef5e5233d28bc005a1d3cec5803
Fie Langmann <fielangmann@gmail.com>
2024-09-09 08:48:06 +0000

    updated definition of covariates for all analyses

--- R/analyses.R
+++ R/analyses.R
@@ -19,59 +19,43 @@ define_exposure_variables <- function(data) {
 
 # Main analyses -----------------------------------------------------------
 main_model1 <- function(data) {
-  covariates_model1 <- all.vars(
-    ~ # other food components
-      cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-      dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-      veggie_weekly + potato_weekly + egg_weekly +
-      non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-      sauce_weekly + food_weight_weekly +
-      # baseline strata
-      strata(region, age_strata, sex)
-  )
-covars1 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
-            "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
-            "veggie_weekly", "potato_weekly", "egg_weekly",
-            "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
-            "sauce_weekly", "food_weight_weekly", "strata(region, age_strata, sex)")
-  
-  
+  covars1 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+               "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+               "veggie_weekly", "potato_weekly", "egg_weekly",
+               "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+               "sauce_weekly", "food_weight_weekly", "strata(region, age_strata, sex)")
+
+
   model1_formulas <- list(
-    meat_model1 = create_formula(c("legumes80", "poultry80", "fish80"), covariates_model1),
-    poultry_model1 = create_formula(c("legumes80", "meats80", "fish80"), covariates_model1),
-    fish_model1 = create_formula(c("legumes80", "meats80", "poultry80"), covariates_model1)
+    meat_model1 = create_formula(c("legumes80", "poultry80", "fish80"), covars1),
+    poultry_model1 = create_formula(c("legumes80", "meats80", "fish80"), covars1),
+    fish_model1 = create_formula(c("legumes80", "meats80", "poultry80"), covars1)
   )
 
   model1_results <- model1_formulas |>
     map(~ coxph(.x, data = data, ties = "breslow")) |>
     map2(names(model1_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
-      mutate(across(where(is.numeric), ~ round(.x, 2))) |>
-      mutate(model = .y))
+           mutate(across(where(is.numeric), ~ round(.x, 2))) |>
+           mutate(model = .y))
 
   return(model1_results)
 }
 
 main_model2<- function(data) {
-  covariates_model2 <- (
-    ~ # other food components
-      cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-      dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-      veggie_weekly + potato_weekly + egg_weekly +
-      non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-      sauce_weekly + food_weight_weekly +
-      # other variables
-      alc_spline + ethnicity + deprivation + education +
-      cohabitation + physical_activity + smoking +
-      related_disease + disease_family + yearly_income +
-      # baseline strata
-      strata(region, age_strata, sex)
-  ) |>
-    all.vars()
+  covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+               "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+               "veggie_weekly", "potato_weekly", "egg_weekly",
+               "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+               "sauce_weekly", "food_weight_weekly", "alc_spline", "ethnicity",
+               "deprivation", "education", "cohabitation", "physical_activity",
+               "smoking", "related_disease", "disease_family", "yearly_income",
+               "strata(region, age_strata, sex)")
+
 
   model2_formulas <- list(
-    meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covariates_model2),
-    poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covariates_model2),
-    fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covariates_model2)
+    meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covars2),
+    poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covars2),
+    fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
   )
 
   model2_results <- model2_formulas |>
@@ -84,28 +68,20 @@ main_model2<- function(data) {
 }
 
 main_model3<- function(data) {
-  covariates_model3 <- (
-    ~ # other food components
-      cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-      dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-      veggie_weekly + potato_weekly + egg_weekly +
-      non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-      sauce_weekly + food_weight_weekly +
-      # other variables
-      alc_spline + ethnicity + deprivation + education +
-      cohabitation + physical_activity + smoking +
-      related_disease + disease_family + yearly_income +
-      # anthropometry
-      bmi30 +
-      # baseline strata
-      strata(region, age_strata, sex)
-  ) |>
-    all.vars()
+  covars3 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+               "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+               "veggie_weekly", "potato_weekly", "egg_weekly",
+               "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+               "sauce_weekly", "food_weight_weekly", "alc_spline", "ethnicity",
+               "deprivation", "education", "cohabitation", "physical_activity",
+               "smoking", "related_disease", "disease_family", "yearly_income",
+               "bmi30", "strata(region, age_strata, sex)")
+
 
   model3_formulas <- list(
-    meat_model3 = create_formula(c("legumes80", "poultry80", "fish80"), covariates_model3),
-    poultry_model3 = create_formula(c("legumes80", "meats80", "fish80"), covariates_model3),
-    fish_model3 = create_formula(c("legumes80", "meats80", "poultry80"), covariates_model3)
+    meat_model3 = create_formula(c("legumes80", "poultry80", "fish80"), covars3),
+    poultry_model3 = create_formula(c("legumes80", "meats80", "fish80"), covars3),
+    fish_model3 = create_formula(c("legumes80", "meats80", "poultry80"), covars3)
   )
 
   model3_results <- model3_formulas |>
@@ -123,35 +99,29 @@ consumers_analyses<- function(data) {
   consumers <- data %>%
     subset(legume_weekly > 0)
 
-  covariates_model2 <- (
-    ~ # other food components
-      cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-      dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-      veggie_weekly + potato_weekly + egg_weekly +
-      non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-      sauce_weekly + food_weight_weekly +
-      # other variables
-      alc_spline + ethnicity + deprivation + education +
-      cohabitation + physical_activity + smoking +
-      related_disease + disease_family + yearly_income +
-      # baseline strata
-      strata(region, age_strata, sex)
-  ) |>
-    all.vars()
+  covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+               "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+               "veggie_weekly", "potato_weekly", "egg_weekly",
+               "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+               "sauce_weekly", "food_weight_weekly", "alc_spline", "ethnicity",
+               "deprivation", "education", "cohabitation", "physical_activity",
+               "smoking", "related_disease", "disease_family", "yearly_income",
+               "strata(region, age_strata, sex)")
 
-    model2_formulas <- list(
-      meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covariates_model2),
-      poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covariates_model2),
-      fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covariates_model2)
-    )
 
-    model2_results_3recalls <- model2_formulas |>
+  model2_formulas <- list(
+    meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covars2),
+    poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covars2),
+    fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
+  )
+
+    model2_results_consumers <- model2_formulas |>
       map(~ coxph(.x, data = consumers, ties = "breslow")) |>
       map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
         mutate(across(where(is.numeric), ~ round(.x, 2))) |>
         mutate(model = .y))
 
-    return(model_results_3recalls)
+    return(model2_results_consumers)
 }
 
 total_intake <- function(data) {
@@ -183,21 +153,14 @@ total_intake <- function(data) {
 # sensitivity analyses ----------------------------------------------------
 # peas included in legume component
 legumes_and_peas<- function(data) {
-  covariates_pea <- (
-    ~ # other food components
-      cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-      dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-      veggie_pea_weekly + potato_weekly + egg_weekly +
-      non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-      sauce_weekly + food_weight_weekly +
-      # other variables
-      alc_spline + ethnicity + deprivation + education +
-      cohabitation + physical_activity + smoking +
-      related_disease + disease_family + yearly_income +
-      # baseline strata
-      strata(region, age_strata, sex)
-  ) |>
-    all.vars()
+  covariates_pea <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+                      "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+                      "veggie_pea_weekly", "potato_weekly", "egg_weekly",
+                      "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+                      "sauce_weekly", "food_weight_weekly", "alc_spline", "ethnicity",
+                      "deprivation", "education", "cohabitation", "physical_activity",
+                      "smoking", "related_disease", "disease_family", "yearly_income",
+                      "strata(region, age_strata, sex)")
 
   model2_formulas_pea <- list(
     meat_model2 = create_formula(c("legumepea80", "poultry80", "fish80"), covariates_pea),
@@ -225,21 +188,14 @@ legumes_without_soy<- function(data) {
     )
 
 
-  covariates_nosoy <- (
-    ~ # other food components
-      cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-      dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-      veggie_weekly + potato_weekly + egg_weekly +
-      non_alc_beverage_soymilk_weekly + alc_beverage_weekly + snack_weekly +
-      sauce_weekly + food_weight_weekly +
-      # other variables
-      alc_spline + ethnicity + deprivation + education +
-      cohabitation + physical_activity + smoking +
-      related_disease + disease_family + yearly_income +
-      # baseline strata
-      strata(region, age_strata, sex)
-  ) |>
-    all.vars()
+  covariates_nosoy <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+                        "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+                        "veggie_weekly", "potato_weekly", "egg_weekly",
+                        "non_alc_beverage_soymilk_weekly", "alc_beverage_weekly", "snack_weekly",
+                        "sauce_weekly", "food_weight_weekly", "alc_spline", "ethnicity",
+                        "deprivation", "education", "cohabitation", "physical_activity",
+                        "smoking", "related_disease", "disease_family", "yearly_income",
+                        "strata(region, age_strata, sex)")
 
   model2_formulas_nosoy <- list(
     meat_model2 = create_formula(c("legumenosoy80", "poultry80", "fish80"), covariates_nosoy),
@@ -262,26 +218,19 @@ alcohol_restricted_analyses <- function(data) {
   lower_alc <- data %>%
     subset(alcohol_weekly < percentile_90)
 
-  covariates_model2 <- (
-    ~ # other food components
-      cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-      dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-      veggie_weekly + potato_weekly + egg_weekly +
-      non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-      sauce_weekly + food_weight_weekly +
-      # other variables
-      alc_spline + ethnicity + deprivation + education +
-      cohabitation + physical_activity + smoking +
-      related_disease + disease_family + yearly_income +
-      # baseline strata
-      strata(region, age_strata, sex)
-  ) |>
-    all.vars()
+  covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+               "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+               "veggie_weekly", "potato_weekly", "egg_weekly",
+               "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+               "sauce_weekly", "food_weight_weekly", "alc_spline", "ethnicity",
+               "deprivation", "education", "cohabitation", "physical_activity",
+               "smoking", "related_disease", "disease_family", "yearly_income",
+               "strata(region, age_strata, sex)")
 
   model2_formulas <- list(
-    meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covariates_model2),
-    poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covariates_model2),
-    fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covariates_model2)
+    meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covars2),
+    poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covars2),
+    fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
   )
 
   model2_results_alc <- model2_formulas |>
@@ -298,26 +247,19 @@ normal_liver_analyses<- function(data) {
   normal_liver <- data %>%
     subset(alt < 40)
 
-  covariates_model2 <- (
-    ~ # other food components
-      cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-      dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-      veggie_weekly + potato_weekly + egg_weekly +
-      non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-      sauce_weekly + food_weight_weekly +
-      # other variables
-      alc_spline + ethnicity + deprivation + education +
-      cohabitation + physical_activity + smoking +
-      related_disease + disease_family + yearly_income +
-      # baseline strata
-      strata(region, age_strata, sex)
-  ) |>
-    all.vars()
+  covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+               "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+               "veggie_weekly", "potato_weekly", "egg_weekly",
+               "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+               "sauce_weekly", "food_weight_weekly", "alc_spline", "ethnicity",
+               "deprivation", "education", "cohabitation", "physical_activity",
+               "smoking", "related_disease", "disease_family", "yearly_income",
+               "strata(region, age_strata, sex)")
 
   model2_formulas <- list(
-    meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covariates_model2),
-    poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covariates_model2),
-    fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covariates_model2)
+    meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covars2),
+    poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covars2),
+    fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
   )
 
   model2_results_liver <- model2_formulas |>
@@ -334,26 +276,19 @@ three_recalls_analyses <- function(data) {
   data3 <- data %>%
     subset(p20077 >= 3)
 
-  covariates_model2 <- (
-    ~ # other food components
-      cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-      dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-      veggie_weekly + potato_weekly + egg_weekly +
-      non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-      sauce_weekly + food_weight_weekly +
-      # other variables
-      alc_spline + ethnicity + deprivation + education +
-      cohabitation + physical_activity + smoking +
-      related_disease + disease_family + yearly_income +
-      # baseline strata
-      strata(region, age_strata, sex)
-  ) |>
-    all.vars()
+  covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+               "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+               "veggie_weekly", "potato_weekly", "egg_weekly",
+               "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+               "sauce_weekly", "food_weight_weekly", "alc_spline", "ethnicity",
+               "deprivation", "education", "cohabitation", "physical_activity",
+               "smoking", "related_disease", "disease_family", "yearly_income",
+               "strata(region, age_strata, sex)")
 
   model2_formulas <- list(
-    meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covariates_model2),
-    poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covariates_model2),
-    fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covariates_model2)
+    meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covars2),
+    poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covars2),
+    fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
   )
 
   model2_results_3recalls <- model2_formulas |>
