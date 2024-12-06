d538335410e007d3428776c8677805747f76156b
Fie Langmann <fielangmann@gmail.com>
2024-12-06 14:25:34 +0000

    added test for linearity assumption for substitution model

--- R/additional_calculations.R
+++ R/additional_calculations.R
@@ -1,13 +1,10 @@
 # Additional calculations
 
+# Time between completed diet questionnaires ------------------------------
+
 library(dplyr)
 library(magrittr)
 library(tidyverse)
-
-
-
-# Calculate differences between completed questionnaire dates for each participant
-
 data <- targets::tar_read(eligible_participants)
 
 data <- data %>% mutate(
@@ -45,7 +42,12 @@ cat("SD of interval:", sd_interval, "days\n")
 
 
 
-# printing knots in alcohol splines
+
+# Printing knots of alc_spline --------------------------------------------
+library(dplyr)
+library(magrittr)
+library(tidyverse)
+
 data <- targets::tar_read(id_data)
 
 data <- data %>% mutate(
@@ -68,3 +70,69 @@ knots <- attr(spline_basis, "knots")
 cat("knots are placed at:", knots, "\n")
 
 
+
+# Testing linearity assumption --------------------------------------------
+# investigating the association between weekly intakes of legumes, red and processed meat,
+# poultry, and fish and NAFLD for non-linearity using restricted cubic splines at the
+# 10th, 50th and 90th percentiles as proposed in: https://pubmed.ncbi.nlm.nih.gov/37694448/
+library(dplyr)
+library(magrittr)
+library(tidyverse)
+library(survival)
+library(splines)
+library(broom)
+
+# Calculate the percentiles for the knots
+knots_legume <- quantile(data$legume_weekly, probs = c(0.1, 0.5, 0.9), na.rm = TRUE)
+knots_meat <- quantile(data$meats_weekly, probs = c(0.1, 0.5, 0.9), na.rm = TRUE)
+knots_poultry <- quantile(data$poultry_weekly, probs = c(0.1, 0.5, 0.9), na.rm = TRUE)
+knots_fish <- quantile(data$fish_weekly, probs = c(0.1, 0.5, 0.9), na.rm = TRUE)
+
+# Generate the spline basis with specified knots
+data <- data %>% mutate(
+  legume_spline = splines::bs(legume_weekly, knots = knots_legume, degree = 3),
+  meat_spline = splines::bs(meats_weekly, knots = knots_meat, degree = 3),
+  poultry_spline = splines::bs(poultry_weekly, knots = knots_poultry, degree = 3),
+  fish_spline = splines::bs(fish_weekly, knots = knots_fish, degree = 3)
+)
+
+
+## legumes -----------------------------------------------------------------
+
+
+## meat -----------------------------------------------------------------
+meat <- coxph(Surv(survival_time, event = nafld) ~ meats_weekly + alc_spline + ethnicity
+              + deprivation + education + cohabitation + physical_activity + smoking
+              + related_disease + disease_family + yearly_income + strata(region, age_strata, sex),
+              data = data,
+              ties = 'breslow') %>%
+  tidy()
+
+meat_splines <- coxph(Surv(survival_time, event = nafld) ~ meat_spline + alc_spline + ethnicity
+                      + deprivation + education + cohabitation + physical_activity + smoking
+                      + related_disease + disease_family + yearly_income + strata(region, age_strata, sex),
+                      data = data,
+                      ties = 'breslow') %>%
+  tidy()
+
+
+
+# Fit the Cox models (you have already done this)
+meat <- coxph(Surv(survival_time, event = nafld) ~ meats_weekly + alc_spline + ethnicity
+              + deprivation + education + cohabitation + physical_activity + smoking
+              + related_disease + disease_family + yearly_income + strata(region, age_strata, sex),
+              data = data,
+              ties = 'breslow')
+
+meat_splines <- coxph(Surv(survival_time, event = nafld) ~ meat_spline + alc_spline + ethnicity
+                      + deprivation + education + cohabitation + physical_activity + smoking
+                      + related_disease + disease_family + yearly_income + strata(region, age_strata, sex),
+                      data = data,
+                      ties = 'breslow')
+
+# Perform a likelihood ratio test
+lrt_result <- anova(meat, meat_splines, test = "LRT") %>% print()
+
+## poultry -----------------------------------------------------------------
+
+## fish -----------------------------------------------------------------
