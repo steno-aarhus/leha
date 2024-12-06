a71937df0d21d13f7d10a3e5f4e6d091a4eda5ad
Fie Langmann <fielangmann@gmail.com>
2024-12-06 12:59:17 +0000

    added printing of alc_spline knots

--- R/additional_calculations.R
+++ R/additional_calculations.R
@@ -1,9 +1,13 @@
-# Calculate differences between completed questionnaire dates for each participant
-
+# Additional calculations
 
 library(dplyr)
 library(magrittr)
 library(tidyverse)
+
+
+
+# Calculate differences between completed questionnaire dates for each participant
+
 data <- targets::tar_read(eligible_participants)
 
 data <- data %>% mutate(
@@ -38,3 +42,29 @@ sd_interval <- sd(all_differences)
 # Print results
 cat("Mean interval:", mean_interval, "days\n")
 cat("SD of interval:", sd_interval, "days\n")
+
+
+
+# printing knots in alcohol splines
+data <- targets::tar_read(id_data)
+
+data <- data %>% mutate(
+  alcohol_intake = rowSums(pick(matches("p26030")), na.rm = TRUE),
+  alcohol_daily = alcohol_intake / p20077,
+  alcohol_weekly = alcohol_daily * 7,
+  alc_spline = splines::bs(alcohol_weekly, knots = 4, degree = 3)
+)
+
+# Calculate internal knots based on quantiles
+nknots <- 4  # Number of knots (adjust as needed)
+internal_knots <- quantile(data$alcohol_weekly, probs = seq(0.25, 0.75, length.out = nknots), na.rm = TRUE)
+
+# Generate the spline basis using specified knots
+spline_basis <- splines::bs(data$alcohol_weekly, knots = internal_knots, degree = 3)
+
+# Extract and print the knot positions
+knots <- attr(spline_basis, "knots")
+
+cat("knots are placed at:", knots, "\n")
+
+
