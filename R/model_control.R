77464290ad1159e926ac0dcdd2aa9804e515f15a
Fie-Langmann <113172774+Fie-Langmann@users.noreply.github.com>
2026-02-12 14:53:05 +0100

    Added model assumption when yearly income removed from model2 adjustment

--- R/model_control.R
+++ R/model_control.R
@@ -76,3 +76,94 @@ model_assumption <- function(data) {
   ph_test3 <- cox.zph(fish_model2) %>% print()
   return(data)
 }
+
+# yearly income appears to violate proportionality both visually and with p-value.
+# I will try to remove from model and assess proportionality and whether estimates changes:
+
+
+data <- data %>%
+    mutate(
+        legumes80 = legume_weekly / 80,
+        meats80 = meats_weekly / 80,
+        poultry80 = poultry_weekly / 80,
+        fish80 = fish_weekly / 80
+    )
+meat_model2 <- coxph(
+    Surv(survival_time, nafld == 1) ~
+        # removing meat
+        legumes80 + poultry80 + fish80 +
+        # other food components
+        cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
+        dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
+        veggie_weekly + potato_weekly + egg_weekly +
+        non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
+        sauce_weekly + food_weight_weekly +
+      # other variables
+      alcohol_weekly + ethnicity + deprivation + education +
+      cohabitation + physical_activity + smoking +
+      related_disease + disease_family +
+      strata(region, age_strata, sex),
+    data = data, ties = "breslow"
+  )
+
+ph_test1 <- cox.zph(meat_model2)
+print(ph_test1)
+plot(ph_test1)
+
+meat_results <- tidy(meat_model2, exponentiate = TRUE, conf.int = TRUE) %>%
+    mutate(across(where(is.numeric), ~ round(.x, 2)))
+
+
+# poultry
+poultry_model2 <- coxph(
+    Surv(survival_time, nafld == 1) ~
+        # removing meat
+        legumes80 + meats80 + fish80 +
+        # other food components
+        cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
+        dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
+        veggie_weekly + potato_weekly + egg_weekly +
+        non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
+        sauce_weekly + food_weight_weekly +
+      # other variables
+      alcohol_weekly + ethnicity + deprivation + education +
+      cohabitation + physical_activity + smoking +
+      related_disease + disease_family +
+      strata(region, age_strata, sex),
+    data = data, ties = "breslow"
+  )
+
+
+ph_test2 <- cox.zph(poultry_model2)
+print(ph_test2)
+plot(ph_test2)
+
+poultry_results <- tidy(poultry_model2, exponentiate = TRUE, conf.int = TRUE) %>%
+    mutate(across(where(is.numeric), ~ round(.x, 2)))
+
+# fish
+fish_model2 <- coxph(
+    Surv(survival_time, nafld == 1) ~
+        # removing meat
+        legumes80 + meats80 + poultry80 +
+        # other food components
+        cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
+        dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
+        veggie_weekly + potato_weekly + egg_weekly +
+        non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
+        sauce_weekly + food_weight_weekly +
+      # other variables
+      alcohol_weekly + ethnicity + deprivation + education +
+      cohabitation + physical_activity + smoking +
+      related_disease + disease_family +
+      strata(region, age_strata, sex),
+    data = data, ties = "breslow"
+  )
+
+
+ph_test3 <- cox.zph(fish_model2)
+print(ph_test3)
+plot(ph_test3)
+
+fish_results <- tidy(fish_model2, exponentiate = TRUE, conf.int = TRUE) %>%
+    mutate(across(where(is.numeric), ~ round(.x, 2)))
